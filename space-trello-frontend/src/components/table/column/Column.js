edd34e516b4594f5815824190a5336087fc120ca
M1loseph <milosz.dyka@gmail.com>
2021-05-25 00:02:57 +0200

    editing column name

--- space-trello-frontend/src/components/table/column/Column.js
+++ space-trello-frontend/src/components/table/column/Column.js
@@ -1,13 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import ColumnFooter from "./ColumnFooter";
 import ColumnHeader from "./ColumnHeader";
 import Card from "./card/Card";
 
 const Column = ({ column }) => {
-  const { title, cards } = column;
+  const { cards } = column;
+  const [title, setTitle] = useState("Hello world");
   return (
     <div className={"p-2 d-flex flex-column trello-column rounded mh-100"}>
-      <ColumnHeader title={title} />
+      <ColumnHeader title={title} setTitle={setTitle} />
       <div className="overflow-auto">
         {cards.map((c) => (
           <Card key={c.id} card={c} />
--- space-trello-frontend/src/components/table/column/ColumnHeader.js
+++ space-trello-frontend/src/components/table/column/ColumnHeader.js
@@ -1,14 +1,31 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import ColumnDropdown from "./ColumnDropdown";
 import ColumnOptionsHeader from "./ColumnOptionsHeader";
 import OptionsListElement from "./OptionsListElement";
 
-const ColumnHeader = ({ title }) => {
+const ColumnHeader = ({ title, setTitle }) => {
   const [showOptions, setShowOptions] = useState(false);
-
+  const [disabledEdit, setDisabledEdit] = useState(true);
+  useEffect(() => {
+    console.log(disabledEdit);
+  }, [disabledEdit]);
   return (
     <div className="d-flex flex-row justify-content-between ">
-      <h4 className={"font-weight-bold"}>{title}</h4>
+      <input
+        type="text"
+        className="h4 trello-table-name "
+        value={title}
+        readOnly={disabledEdit}
+        onDoubleClick={() => setDisabledEdit(false)}
+        onChange={(e) => setTitle(e.target.value)}
+        onBlur={() => setDisabledEdit(true)}
+        onKeyPress={(e) => {
+          const { key } = e;
+          if (key === "Enter") {
+            setDisabledEdit(true);
+          }
+        }}
+      />
       <div className="trello-dropdown-toggler ">
         <i
           onClick={() => setShowOptions(!showOptions)}
