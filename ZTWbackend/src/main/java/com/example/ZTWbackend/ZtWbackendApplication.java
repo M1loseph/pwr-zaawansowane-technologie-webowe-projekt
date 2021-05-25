0df6b773e340f00e75dea9a15e19db492c872b70
scizla <sebastian.cizla@capgemini.com>
2021-05-26 00:18:42 +0200

    new authentication

--- ZTWbackend/src/main/java/com/example/ZTWbackend/ZtWbackendApplication.java
+++ ZTWbackend/src/main/java/com/example/ZTWbackend/ZtWbackendApplication.java
@@ -1,7 +1,32 @@
 package com.example.ZTWbackend;
 
+import com.example.ZTWbackend.filters.JwtRequestFilter;
+import com.example.ZTWbackend.model.AuthenticationRequest;
+import com.example.ZTWbackend.model.AuthenticationResponse;
+import com.example.ZTWbackend.services.MyUserDetailsService;
+import com.example.ZTWbackend.util.JwtUtil;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class ZtWbackendApplication {
